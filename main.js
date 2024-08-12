46f710fe936593c18a830888e2aa14744fbf4242
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-08-12 13:19:11 +0530

    create the main body of the project & connect with mongo db

---
+++ main.js
@@ -0,0 +1,24 @@
+//imports
+require("dotenv").config();
+const express =  require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+const app = express();
+const PORT =process.env.PORT || 4000;
+
+//database connection 
+mongoose.connect(process.env.DB_URI );
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", ()=> console.log("Connected to the database"));
+
+
+
+app.get("/", (req,res)=>{
+    res.send("Hello world");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at http://localhost:${PORT}`);
+})
